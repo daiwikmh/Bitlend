8346e800d06f59523b01eaaa2cffb0cbcedd174f
daiwikmh <daiwikdomain@gmail.com>
2025-06-18 23:30:35 +0530

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router';
-import ChatInterface from '@/components/Chat/ChatInterface';
-import LandingPage from './components/landing-page/Landing';
+import { toHex } from "viem";
+import client from "@story-protocol/core-sdk"
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/chat" element={<ChatInterface />} />
-    </Routes>
-  );
-}
+const response = await client.ipAsset.register({
+  nftContract: "0x041B4F29183317Fd352AE57e331154b73F8a1D73",
+  tokenId: "12",
+  ipMetadata: {
+    ipMetadataURI: "test-uri",
+    ipMetadataHash: toHex("test-metadata-hash", { size: 32 }),
+    nftMetadataHash: toHex("test-nft-metadata-hash", { size: 32 }),
+    nftMetadataURI: "test-nft-uri",
+  },
+});
 
-export default App;
+console.log(
+  `Root IPA created at transaction hash ${response.txHash}, IPA ID: ${response.ipId}`
+)
